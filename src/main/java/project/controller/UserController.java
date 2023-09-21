5db256c73d736f43dc6b527a23ffbf146b20946a
Artem Sadovenko <asadovenko7@gmail.com>
2023-09-21 17:45:07 +0300

    added user CRUD

---
+++ src/main/java/project/controller/UserController.java
@@ -0,0 +1,32 @@
+package project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.dto.UserDto;
+import project.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public List<UserDto> getAll(){
+        return userService.getAll();
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestBody UserDto user){
+        userService.create(user);
+    }
+
+    @PutMapping("/")
+    public void update(@RequestBody UserDto user){
+        userService.update(user);
+    }
+}
